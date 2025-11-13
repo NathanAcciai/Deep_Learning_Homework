d5e2bc9d9dbdf6946ecbe8a294896d9b8c429db2
Nathan <nathanacciai@libero.it>
2025-11-13 15:09:31 +0100

    update Exercise 3

--- Homework_2/Q_Learining.py
+++ Homework_2/Q_Learining.py
@@ -89,6 +89,117 @@ class QNetwork(nn.Module):
         )
     def forward(self,x):
         return self.net(x)
+
+
+'''Definiamo adesso l'agente che sfrutta il Q-Learning
+Definisco anche una epsilon per l'esplorazione in modo tale da avere un comportamento più esplorativo all' inziio
+mentre andando avanti riduco portando la rete a un comportameneto più deterministico e quindi selezioni di azioni migliori.
+Si parte con un epsilon alto quindi azione casuale e piano piano va a descrescere prendendo azioni sempre migliori.
+'''
+'''
+Piccolo sunto della rete target per ricordare il motivo del perchè risulta essere una copia della rete online.
+La rete target serve a dare un obbiettivo stabile alla rete target anche se questo è abbastanza distante dalla realtà perchè 
+il problema del RL era che ogni volta la rete cambiava i propri target dunque era come avvicinarsi a un target che successivamnete si sarebbe mosso
+questo ovviamente portava molta instabilità, ma con l'aggiunta della rete target e con un lr sufficientemente basso allora nache se essa diverge dalla realtà
+quindi diverge dalla stima vera del target comunque offre alla rete online un ancora sotto cui poter convergere e quindi con l'aggiornamento della rete
+target anche l'errore rispetto alla realtà va a diminuire.
+Questo anche perchè nel buffer si ha una storia delle esperinze e quindi andando comunque a riempirlo con nuove esperienze dove anche la scelta delle azioni diventa sempre
+più deterministica.
+Infatti inizialmente si ha un alta esplorazione per cercare le azioni migliori fino a migliorare sulla base delle esperienze avute
+Mnih, Volodymyr, et al. "Human-level control through deep reinforcement learning." nature 518.7540 (2015): 529-533.
+'''
+
+class DQNAgent:
+    def __init__(self,
+                 obs_dim,
+                 n_action,
+                 replay_buffer: ReplayBuffer,
+                 lr=0.001,
+                 gamma=0.99,
+                 batch_size=16,
+                 epsilon_start=1.0,
+                 epsilon_end=0.05,
+                 epsilon_decay= 10000,
+                 device="cpu",
+                 target_update_freq= 1000):
+        
+        self.device= device
+        self.replay_buffer= replay_buffer
+        self.n_action= n_action
+        self.lr= lr
+        self.gamma= gamma
+        self.batch_size= batch_size
+        self.espilon_start= epsilon_start
+        self.epislon_end=epsilon_end
+        self.epsilon_decay= epsilon_decay
+        self.target_update_freq= target_update_freq
+        self.total_step=0
+
+        #Rete online si aggiorna passo dopo passo e vi calcolo la LOSS
+        self.q_network= QNetwork(obs_dim=obs_dim, n_action=n_action).to(device)
+        #rete che viene aggiornata periodicamente e serve per stabilizzare l'addestramento 
+        #poichè mi da il massimo ritorno atteso nello stato successivo
+        self.q_target= QNetwork(obs_dim=obs_dim, n_action=n_action).to(device)
+        #partono con gli stessi pesi 
+        self.q_target.load_state_dict(self.q_network)
+
+        self.optimizer= optim.AdamW(params=self.q_network.parameters(), lr=lr)
+        self.epsilon= epsilon_start
+
+    #aggiorno la epsilon in modo da avere un carattere più stocastico all' inizio quindi di scelta casuale e maggiroe esplorazione
+    #Piano piano la epsilon decresce del fattore diventando al minimo 0.05 e quindi porta a un comportamento deterministico
+    def update_epsilon(self):
+        self.epsilon = max(
+            self.epsilon_end,
+            self.epsilon_start - (self.epsilon_start - self.epsilon_end) * (self.total_steps / self.epsilon_decay)
+        )
+
+    def take_action(self,state):
+        self.total_step += 1
+        self.update_epsilon()
+
+        #numero casuale tra 0 e 1 , fa si che quando epsilon scende allora avrò sempre meno esplorazioni.
+        if np.random.random() < self.epsilon:
+            return np.random.randint(self.n_action)
+        else:
+            state= torch.tensor(state, device=self.device).unsqueeze(0)
+            q_values= self.q_network(state)
+            return int(torch.argmax(q_values, dim=1).item())
+    '''da ricordare non si mette model.train o eval perchè non ho dropout ne batch norm essendo che la rete è un semplice MLP
+    ''' 
+    def training_step(self):
+        #prima di tutto devo controllare se il replay buffer ha una dimensione pari a un batch 
+        if len(self.replay_buffer)< self.batch_size:
+            return
+        
+        obs,action, reward,next_obs,done =self.replay_buffer.sample(self.batch_size)
+        #ritorno atteso della rete principale
+        q_values= self.q_network(obs)
+        #essendo che ho un tensore della forma [batch_size, num_action], devo prendere le azioni che sono state intraprese con [batch,1]
+        #Prendo le azioni del sample perchè ho la descrizione dell' esperienza avuta
+        q_values= q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+
+        with torch.no_grad():
+            #calcolo le ricompense target
+            next_q= self.q_target(next_obs)
+            #prendo la ricompensa massima
+            next_q_max= next_q.max(dim=1)[0]
+            #ora calolco la ricompensa target y
+            # r_t + gamma (1-d_t) * max Q(s_t+1,a')
+            target= reward + self.gamma* (1-done)*next_q
+        #ora che ho la ricompensa target posso calcolare MSE per la loss rispetto a quella della rete
+        loss= nn.functional.mse_loss(q_values, target)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        #aggiorno la rete target in modo lento in maniera che abbia stabilità durante il training
+        
+        if self.total_step % self.target_update_freq==0:
+            self.q_target.load_state_dict(self.q_network.state_dict())
+
+
+
     
 
 
